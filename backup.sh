d5f9bcc34ab416ed2c4834a9cc46a131e7fe73b5
Samuel Terburg <Samuel.Terburg@Panther-IT.nl>
2017-04-19 13:23:03 +0000

    Upgraded to ocp v3.5 (s/petsets/statefullsets/; s/scheduledjobs/cronjobs/)

--- backup.sh
+++ backup.sh
@@ -15,7 +15,7 @@ do
   cd $i
   oc export namespace $i >ns.yml
   oc export project   $i >project.yml
-  for j in pods replicationcontrollers deploymentconfigs buildconfigs services routes pvc quota hpa secrets configmaps daemonsets deployments endpoints imagestreams ingress jobs limitranges policies policybindings roles rolebindings resourcequotas replicasets serviceaccounts templates oauthclients petsets
+  for j in pods replicationcontrollers deploymentconfigs buildconfigs services routes pvc quota hpa secrets configmaps daemonsets deployments endpoints imagestreams ingress scheduledjobs jobs limitranges policies policybindings roles rolebindings resourcequotas replicasets serviceaccounts templates oauthclients petsets
   do 
     mkdir $j
     cd $j
@@ -91,26 +91,30 @@ do
     [ "$SCHEDULE" == "" ] && continue
     #oc -n $PROJECT env --list $POD |tail -n +2`
     echo "$POD in $PROJECT want a $TECH $SCHEDULE backup"
+    mkdir -p $DIR/../$TECH/$PROJECT  2>/dev/null
     case $TECH in
       mysql)
-        mkdir -p $DIR/../mysql/$PROJECT  2>/dev/null
-        oc -n $PROJECT exec $POD -- /usr/bin/sh -c 'PATH=$PATH:/opt/rh/mysql55/root/usr/bin mysqldump -h 127.0.0.1 -u $MYSQL_USER --password=$MYSQL_PASSWORD $MYSQL_DATABASE' >$DIR/../mysql/$PROJECT/$DC.sql
+        oc -n $PROJECT exec $POD -- /usr/bin/sh -c 'PATH=$PATH:/opt/rh/mysql55/root/usr/bin:/opt/rh/rh-mysql56/root/usr/bin/ mysqldump -h 127.0.0.1 -u $MYSQL_USER --password=$MYSQL_PASSWORD $MYSQL_DATABASE' >$DIR/../mysql/$PROJECT/$DC.sql
+        ;;
+      postgresql)
+        oc -n $PROJECT exec $POD -- /usr/bin/sh -c 'PATH=$PATH:/opt/rh/rh-postgresql94/root/usr/bin pg_dump $POSTGRESQL_DATABASE' >$DIR/../postgresql/$PROJECT/$DC.sql
         ;;
       *)
         echo "Unknown technology"
         ;;
     esac
+    gzip $DIR/../mysql/$PROJECT/$DC.sql
 done
 
 
 
 ### Persistent Volumes ###
-cd $DIR/volumes
+cd $DIR/../volumes
 mount -t glusterfs 192.168.0.20:vol_e22f1c43ac7556b3338ba400a61f719e /mnt/tmp/
-rsync -va /mnt/tmp uploads
+rsync -va /mnt/tmp/ uploads/
 umount /mnt/tmp
 mount -t glusterfs 192.168.0.20:pvc-b18c0d5d-eb28-11e6-9f5a-06f5e3517bb5 /mnt/tmp/
-rsync -va /mnt/tmp downloads
+rsync -va /mnt/tmp/ downloads/
 umount /mnt/tmp
 
 
